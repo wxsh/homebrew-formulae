38251a1903a2dc2ff9d1197af51a761071a78066
Andreas Karlsvik <andreas.ocularis@gmail.com>
2020-11-27 13:19:27 +0100

    Update spacebar.rb

--- spacebar.rb
+++ spacebar.rb
@@ -1,8 +1,8 @@
 class Spacebar < Formula
   desc "A minimal status bar for macOS."
-  homepage "https://github.com/cmacrae/spacebar"
-  version "1.2.1"
-  url "https://github.com/cmacrae/spacebar/archive/v#{version}.tar.gz"
+  homepage "https://github.com/wxsh/spacebar"
+  version "0.1.0"
+  url "https://github.com/wxsh/spacebar/archive/v#{version}.tar.gz"
   sha256 "2ea4ae4727b6a3a13c590a20254c1fed52dbd8f381f6d733716281497e348beb"
   head "https://github.com/cmacrae/spacebar.git"
 
